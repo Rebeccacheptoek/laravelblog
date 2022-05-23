f8a41fdf4dddf5e376298fadf2046c4e1a512253
Rebeccacheptoek <rebbytoek095@gmail.com>
2022-05-23 08:13:41 +0300

    git commit -m "Update"

---
+++ resources/views/admin/services.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+{{--    table with name, description and action--}}
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Action</th>
+    </tr>
+</table>
+@endsection
